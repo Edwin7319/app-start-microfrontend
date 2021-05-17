145716e68d922077c033c5ca1822a691d7d4cad2
Edwin <edwin.paul.73@gmail.com>
2021-05-17 13:29:58 -0500

    Consumir modulos de app remotas

---
+++ cart/src/bootstrap.js
@@ -0,0 +1,6 @@
+import * as faker from 'faker';
+
+const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+
+document.querySelector('#test-cart')
+    .innerHTML = cartText;
--- cart/src/index.js
+++ cart/src/index.js
@@ -1,6 +1,1 @@
-import * as faker from 'faker';
-
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
-
-document.querySelector('#test-cart')
-.innerHTML = cartText;
+import ('./bootstrap');
