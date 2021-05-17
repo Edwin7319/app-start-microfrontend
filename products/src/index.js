aa1bf53d90bae4d7e5f2827d8cb5a390f12409c0
Edwin <edwin.paul.73@gmail.com>
2021-05-17 12:00:50 -0500

    Agregado front para cart

--- products/src/index.js
+++ products/src/index.js
@@ -1,13 +1,11 @@
-import * as faker from 'faker';
+import faker from 'faker';
 
-const array = [1,2,3,4,5];
+const arrayLength = [1, 1, 1, 1, 1, 1, 1, 1];
 
-const names = array
-.map(
-    (arr) => {
-        const name = faker.commerce.productName();
-        return `<div>${name}</div>`
-    }
-)
+const arrayProductsName = arrayLength
+    .map(
+        (arr) => `<div>${faker.commerce.productName()}</div>`
+    );
 
-document.querySelector('#test-products').innerHTML = names.join('')
+document.querySelector('#test-products')
+    .innerHTML = arrayProductsName.join('');
